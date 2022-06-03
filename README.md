f03e38edc971fb29399dae1d831b4edc33aef310
abdelrhman ahmed kamal <abdelrhman.ahmed.nassar@gmail.com>
2022-06-03 11:46:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Omnifood
+
+## The omnifood amazing website project built with HTML and CSS and java-script
+
+The project is made using Flex-box and grid layout in addition to soma javascript code and it contain: 
+
+- Hero section 
+- featured in section 
+- how it works section 
+- meals section 
+- testimonials section
+- pricing section 
+- call to action section 
+- footer 
+
+
+## [live demo](https://abdelrhman-ahmed-kamal.github.io/omnifood/)
+
+![screen shot](Omnifood.png)
---
+++ js/script.js
@@ -0,0 +1,125 @@
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+/////////////////////////////////////////////////////////////
+// hero section animation
+const hero = document.querySelector(".hero");
+window.addEventListener("load", function () {
+  hero.classList.remove("out");
+});
+
+//////////////////////////////////////////////
+// sections animation
+
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entires, observer) {
+  const [entry] = entires;
+  // the entry.target is the element that has been observed
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-down");
+  // stop observing
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+});
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
